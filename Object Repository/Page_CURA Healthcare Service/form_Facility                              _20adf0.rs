9c5aa47a4695d4c3871890776e0b448bab1b4717
Thong Tran <thong.tran@katalon.com>
2022-04-15 11:39:41 +0700

    init

---
+++ Object Repository/Page_CURA Healthcare Service/form_Facility                              _20adf0.rs
@@ -0,0 +1,389 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>form_Facility                              _20adf0</name>
+   <tag></tag>
+   <elementGuidId>5edac092-0b96-4579-b2ec-9b899254e041</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@action='https://katalon-demo-cura.herokuapp.com/appointment.php#summary']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>form.form-horizontal</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = '
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            ' or . = '
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            ')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>form</value>
+      <webElementGuid>cc585613-9443-4a56-98e6-4ea16daf03d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-horizontal</value>
+      <webElementGuid>ca2f2393-4c11-4c66-bf71-efc53799f87c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>action</name>
+      <type>Main</type>
+      <value>https://katalon-demo-cura.herokuapp.com/appointment.php#summary</value>
+      <webElementGuid>4d43eb2e-059d-4e03-9a19-d34133886f73</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>method</name>
+      <type>Main</type>
+      <value>post</value>
+      <webElementGuid>b108f6b1-3aeb-40da-a133-193a75e058ed</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            </value>
+      <webElementGuid>c8726d5e-f5dc-45b4-ae7b-c2a54b7b7625</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;appointment&quot;)/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/form[@class=&quot;form-horizontal&quot;]</value>
+      <webElementGuid>437fc42c-411a-4d03-9cf0-dbcfc583cc05</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//form[@action='https://katalon-demo-cura.herokuapp.com/appointment.php#summary']</value>
+      <webElementGuid>3d62b299-3e19-4cb2-a554-4c0da9ea5acc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='appointment']/div/div/form</value>
+      <webElementGuid>7d54cb95-896a-4a3b-a13e-16a0d901f963</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Make Appointment'])[2]/following::form[1]</value>
+      <webElementGuid>581ae6ee-a8a6-413b-9b7a-bedcb622c5f7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Make Appointment'])[1]/following::form[1]</value>
+      <webElementGuid>97085b46-df97-444a-ac78-11fb37491ada</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form</value>
+      <webElementGuid>b1e1b01f-1863-4f9c-bd9b-162b847382a5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//form[(text() = '
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            ' or . = '
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            ')]</value>
+      <webElementGuid>c7b8b0f6-6230-493d-9b5f-dadc59a45164</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
